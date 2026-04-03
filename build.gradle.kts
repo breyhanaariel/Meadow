d7374fdca3b25f0f2a2046799ec87813e8907c01
breyhanaariel <breyhanadickenson@gmail.com>
2026-04-03 12:41:36 -0400

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file for Meadow
-plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
-    kotlin("kapt") version "2.0.0" apply false
-}
